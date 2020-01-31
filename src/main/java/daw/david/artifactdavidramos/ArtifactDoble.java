6ed914b39210e451fd0018971134a14cad3963a0
David Ramos <ramos_drh@hotmail.com>
2020-01-31 19:00:25 +0100

    Archivos para el artifact

---
+++ src/main/java/daw/david/artifactdavidramos/ArtifactDoble.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package daw.david.artifactdavidramos;
+
+/**
+ *
+ * @author ramos
+ */
+public class ArtifactDoble {
+    private static final int DOS =  2;
+    
+    public static int devolverDoble(int num){
+        return num*DOS;
+    }
+}
